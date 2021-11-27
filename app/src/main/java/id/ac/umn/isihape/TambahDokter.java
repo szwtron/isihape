b333ff142e2e76bef5b9f534ba0a013d80b6eb04
Stainley <szwtron@gmail.com>
2021-11-27 14:14:08 +0700

    Add Lab rv and functionality

--- app/src/main/java/id/ac/umn/isihape/TambahDokter.java
+++ app/src/main/java/id/ac/umn/isihape/TambahDokter.java
@@ -3,7 +3,6 @@ package id.ac.umn.isihape;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -17,8 +16,6 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.HashMap;
-
-import id.ac.umn.isihape.ui.doctors.DoctorsFragment;
 
 public class TambahDokter extends AppCompatActivity {
     private EditText etNamaDokter, etSpesialisDokter, etAlamatDokter, etHargaDokter;

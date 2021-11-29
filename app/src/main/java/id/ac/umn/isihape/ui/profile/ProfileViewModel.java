42b0b5288c90e84c3d2d54ba7a26d6ce863cea98
Christian Halim <chris.richard.halim@gmail.com>
2021-11-29 21:41:31 +0700

    Fix name patients into profile

--- app/src/main/java/id/ac/umn/isihape/ui/profile/ProfileViewModel.java
+++ app/src/main/java/id/ac/umn/isihape/ui/profile/ProfileViewModel.java
@@ -1,13 +1,13 @@
-package id.ac.umn.isihape.ui.patients;
+package id.ac.umn.isihape.ui.profile;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class PatientsViewModel extends ViewModel {
+public class ProfileViewModel extends ViewModel {
     private MutableLiveData<String> mText;
 
-    public PatientsViewModel() {
+    public ProfileViewModel() {
         mText = new MutableLiveData<>();
         mText.setValue("This is patients fragment");
     }

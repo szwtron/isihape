9067a97358bc5cf56552e3e2d182fab50a43a451
Christian Halim <chris.richard.halim@gmail.com>
2021-11-29 16:00:30 +0700

    fix file name

---
+++ app/src/main/java/id/ac/umn/isihape/ui/qrcode/ScanQR.java
@@ -0,0 +1,18 @@
+package id.ac.umn.isihape.ui.qrcode;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import id.ac.umn.isihape.R;
+
+public class ScanQR extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scan_qr);
+
+
+
+    }
+}

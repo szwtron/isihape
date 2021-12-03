7f8d8bde22d5a21689c2f712591d3bddd4fbef2f
Christian Halim <chris.richard.halim@gmail.com>
2021-12-03 16:50:02 +0700

    Isi nama kalian di help.xml

--- app/src/main/java/id/ac/umn/isihape/ui/help/HelpFragment.java
+++ app/src/main/java/id/ac/umn/isihape/ui/help/HelpFragment.java
@@ -1,9 +1,11 @@
 package id.ac.umn.isihape.ui.help;
 
 import android.os.Bundle;
+import android.text.util.Linkify;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -14,13 +16,18 @@ import id.ac.umn.isihape.ui.doctors.DoctorsViewModel;
 
 public class HelpFragment extends Fragment {
 
+    private TextView tvchris1, tvchris2;
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
         View root = inflater.inflate(R.layout.help, container, false);
 
+        tvchris1 = root.findViewById(R.id.tvAdditionalDesc1);
+        tvchris2 = root.findViewById(R.id.tvAdditionalDesc11);
 
-
+        Linkify.addLinks(tvchris1, Linkify.WEB_URLS);
+        Linkify.addLinks(tvchris2, Linkify.WEB_URLS);
 
         return root;
     }
